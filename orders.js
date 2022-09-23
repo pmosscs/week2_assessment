8c89358520e9598744922aee945f379496566bfb
Peyton Moss <pmoss94@gmail.com>
2022-09-23 10:44:35 -0600

    finished orders

--- orders.js
+++ orders.js
@@ -30,7 +30,19 @@
 */
 
 //CODE HERE
+class Ticket{
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
 
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`the order for customer ${this.customerId} is now ${this.status}`);
+    }
+}
 
 
 /*
@@ -45,6 +57,8 @@
 */
 
 //CODE HERE
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM', 575);
+console.log(firstTicket);
 
 
 /*
@@ -54,3 +68,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking');
+console.log(firstTicket);
